3b99abad78e113ee8bf3b986ee8a6eb671ae7394
dorbarshalom <dorbarshalom@gmail.com>
2026-09-07 10:27:44 +0300

    refactor: simplify index page to redirect directly to general documentation

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,44 +1,8 @@
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-
-import Heading from '@theme/Heading';
-import styles from './index.module.css';
-
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/maintor_general">
-            Get Started
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
+import React from 'react';
+import { Redirect } from '@docusaurus/router';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Maintor Systems Documentation">
-      <HomepageHeader />
-      <main>
-        <div className="container padding-vert--lg text--center">
-          <p>Welcome to the official documentation for Maintor Systems.</p>
-        </div>
-      </main>
-    </Layout>
-  );
+  return <Redirect to={useBaseUrl('/docs/maintor_general')} />;
 }
+
